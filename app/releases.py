628b6f6d8d3cdefcf2a89f3f12d01854898515c5
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-11-24 21:52:24 +0000

    releases report script

---
+++ app/releases.py
@@ -0,0 +1,45 @@
+from pprint import pp
+import pandas as pd
+from github.MainClass import Github
+from github.Organization import Organization
+from github.Team import Team
+from shared.github_extensions.init import init
+from shared.github_extensions.pull_requests import pull_requests_in_date_counters
+from shared.github_extensions.rate_limiter import rate_limiter
+from shared.logger.out import out
+from releases import get_args
+
+def main():
+    args = get_args()
+
+    out.log(f"Releases between [{args.start}] and [{args.end}]")
+    g:Github
+    org:Organization
+    team:Team
+    g, org, team = init(args)
+
+    rate_limiter.CONNECTION = g
+    rate_limiter.check()
+
+    repos = team.get_repos()
+    i = 0
+    t = repos.totalCount
+
+    all_releases = []
+    for r in repos:
+        i = i + 1
+        out.group_start(f"[{i}/{t}] Repository [{r.full_name}]")
+        rate_limiter.check()
+
+        releases = pull_requests_in_date_counters(r, args.start, args.end)
+        all_releases.append(releases)
+        out.debug(releases)
+
+        out.group_end()
+
+    df = pd.DataFrame(all_releases)
+    df.to_markdown(f"{args.filename}.md", index=False)
+
+
+if __name__ == "__main__":
+    main()
