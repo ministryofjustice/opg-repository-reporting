5e93d8ead2ea4a4b780811780400d0c2c3ae2c97
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2022-02-04 10:03:16 +0000

    clean up meta args & stub files

--- app/meta/stub.py
+++ app/meta/stub.py
@@ -3,11 +3,10 @@ from string import Template
 import html
 
 def erb(report_dir:str, report_file_path:str) -> str:
-    """
-    """
+    """ Creates a erb file using data from the report_file_path """
     now = datetime.utcnow().strftime("%Y-%m-%d")
 
-    f = open(report_file_path, 'r')
+    f = open(report_file_path, 'r', encoding='utf-8')
     report = f.read()
     f.close()
 
@@ -34,6 +33,6 @@ This was generated via [this script](https://github.com/ministryofjustice/opg-re
     )
     content = template.substitute(date=now, table=report)
     content = html.unescape(content)
-    f = open(f"{report_dir}/report.html.md.erb", 'w')
+    f = open(f"{report_dir}/report.html.md.erb", 'w', encoding='utf-8')
     f.write(content)
     f.close()
