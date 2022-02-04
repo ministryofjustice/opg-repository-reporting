1f6f5671572b8ed4e9e5e15f0a4709b0df175273
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2022-02-04 18:10:19 +0000

    Add new functions for folder levels (root and app)

--- app/shared/folder.py
+++ app/shared/folder.py
@@ -6,7 +6,16 @@ def timestamp_directory(report_type:str) -> str:
     """ Return the path to a timestamp based directory relative to the app root"""
     timestamp = datetime.utcnow().strftime('%Y-%m-%d-%H%M%S')
 
-    directory = Path( os.path.dirname(__file__ ) + "/../../" ).resolve()
+    directory = root_directory()
     path = Path(f"{directory}/__reports__/{report_type}/{timestamp}")
     os.makedirs(path, exist_ok=True)
     return path.resolve()
+
+
+def app_directory() -> Path:
+    """ Return root application directory """
+    return Path(__file__).parent.parent.resolve()
+
+def root_directory() -> Path:
+    """ Return the root of this code base """
+    return Path(__file__).parent.parent.parent.resolve()
