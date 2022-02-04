d3b2e9be176fce4b620fa1ec871cae59703a971d
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2022-02-04 10:45:10 +0000

    cleanup linting

--- app/shared/folder.py
+++ app/shared/folder.py
@@ -2,10 +2,11 @@ from pathlib import Path
 from datetime import datetime
 import os
 
-def timestamp_directory(type:str = "dependencies") -> str:
-    ts = datetime.utcnow().strftime('%Y-%m-%d-%H%M%S')
+def timestamp_directory(report_type:str = "dependencies") -> str:
+    """ Return the path to a timestamp based directory relative to the app root"""
+    timestamp = datetime.utcnow().strftime('%Y-%m-%d-%H%M%S')
 
-    dir = Path( os.path.dirname(__file__ ) + "/../../" ).resolve()
-    path = Path(f"{dir}/__reports__/{type}/{ts}")
+    directory = Path( os.path.dirname(__file__ ) + "/../../" ).resolve()
+    path = Path(f"{directory}/__reports__/{report_type}/{timestamp}")
     os.makedirs(path, exist_ok=True)
     return path.resolve()
