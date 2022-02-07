b19ffc7bb5be875933a05cfeb4481345f1a80e27
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2022-02-07 13:20:16 +0000

    updated ownership class to handle arrays on things for the data
    basic unit test

--- app/packages/owners/ownership.py
+++ app/packages/owners/ownership.py
@@ -1,11 +1,41 @@
-from __future__ import annotations
+from github import Repository, Team
+from ..shared import has_metafiles, repositories
 
 class Ownership:
     """Class to handle ..."""
-    owners:list(str) = []
+    repositories:list = []
+    # list of all owners (service teams)
+    owners:list = []
+    # list of repositories each team owns
+    owner_repositories:dict = {}
+    # list of repositories each team depends on
+    owner_dependencies:dict = {}
 
-    def add(self, metadata:dict) -> None:
-        """Try to add owner data from meta data to service_teams"""
+
+    def update_owners(self, metadata:dict) -> None:
+        """Add owners"""
+        # add to the list of all owners (service teams)
         self.owners.extend(metadata.get("owners", []))
+        self.owners = list( set( self.owners ) )
+
+    def update_owned_repositories(self, repository:Repository.Repository, metadata:dict) -> None:
+        """Update owned repos"""
+        # now work out repos owned
+        for owner in metadata.get("owners", []):
+            owned = self.owner_repositories.get(owner, [])
+            owned.append(repository.html_url)
+            self.owner_repositories[owner] = owned
+    
+    def update_owned_dependencies(self, metadata:dict) -> None:
+        """Update dependencies"""
+        for owner in metadata.get("owners", []):
+            owned = self.owner_dependencies.get(owner, [])
+            owned.extend( metadata.get("dependencies", []) )
+            self.owner_dependencies[owner] = owned
 
 
+    def add(self, repository:Repository.Repository, metadata:dict) -> None:
+        """Add data"""
+        self.update_owners(metadata)
+        self.update_owned_repositories(repository, metadata)
+        self.update_owned_dependencies(metadata)
---
+++ tests/app/owners/test_owners.py
@@ -0,0 +1,58 @@
+import unittest
+from faker import Faker
+from github.Repository import Repository
+from parameterized import parameterized
+from app.packages import Ownership
+
+
+repo_name_prefix:str = 'ministryofjustice'
+
+class TestOwnership(unittest.TestCase):
+    """TestOwnership"""
+
+    def setUp(self) -> None:
+        self.fake = Faker()
+
+    @parameterized.expand([
+        ("small-valid-repo-set", [
+            {
+                'repo': Repository(None, {}, {'html_url': "opg-lpa"}, True ),
+                'metadata': {
+                    'owners': ["make-an-lpa"],
+                    "dependencies": ["opg-data-lpa"]
+                }
+            },
+            {
+                'repo': Repository(None, {}, {'html_url': "opg-use-an-lpa"}, True ),
+                'metadata': {
+                    'owners': ["use-an-lpa"],
+                    "dependencies": [
+                        "opg-pdf-service",
+                        "opg-data-lpa",
+                        "opg-data-lpa-codes"
+                    ]
+                }
+            },
+            {
+                'repo': Repository(None, {}, {'html_url': "opg-pdf-service"}, True ),
+                'metadata': { 'owners': ["use-an-lpa"]}
+            }
+            
+        ], 2)
+    ])
+    def test_add_owner_count(self, name:str, repos_with_metadata:list, owner_count:int):
+        """Test .add """
+        report:Ownership = Ownership()
+        for repo_meta in repos_with_metadata:
+            report.add( repo_meta.get('repo'), repo_meta.get('metadata') )
+        
+        print(report.owners)
+        print(report.owner_repositories)
+        print(report.owner_dependencies)
+
+        # number of owners and number of keys should match owner count
+        self.assertEqual(owner_count, len(report.owners))
+        self.assertEqual(owner_count, len(report.owner_repositories.keys()))
+
+        
+        
