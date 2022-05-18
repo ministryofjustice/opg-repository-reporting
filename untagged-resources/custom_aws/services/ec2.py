a3274d6cfa384da91f7e67ae32d8316c1ad62373
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2022-05-18 11:14:39 +0100

    using the  _find setup is likely to be the norm, so move to the base class

--- untagged-resources/custom_aws/services/ec2.py
+++ untagged-resources/custom_aws/services/ec2.py
@@ -1,5 +1,4 @@
-import aioboto3
-from pprintpp import pprint
+
 from . import ServiceBase
 from . import ApiCaller
 
@@ -9,7 +8,7 @@ class ServiceEc2(ServiceBase):
     """Handle ec2 related resource calls"""
 
     client_type: str = 'ec2'
-    _describe:dict = {
+    _find:dict = {
         'addresses': ApiCaller('describe_addresses', 'Addresses', 'AllocationId'),
         'flow_logs': ApiCaller('describe_flow_logs', 'FlowLogs', 'ResourceId'),
         'hosts': ApiCaller('describe_hosts', 'Hosts', 'HostId'),
@@ -25,33 +24,10 @@ class ServiceEc2(ServiceBase):
     _kwargs:dict = {
         'images': {'Owners': ['self']}
     }
-    _tags:dict = {
+    _tag_names:dict = {
         'network_interfaces': 'TagSet'
     }
     
-    async def all(self, region_name:str) -> dict:
-        """Return all the arns of all resources in the ec2 scope that don't contain tags"""
-        untagged = []
-        session = aioboto3.Session(region_name=region_name)
-
-        async with session.client(self.client_type, region_name=region_name) as client:
-            for key, instance in self._describe.items():
-                kwargs = self._kwargs.get(key, None)
-                if kwargs is not None:
-                    result = await instance.call(client, **kwargs)
-                else:
-                    result = await instance.call(client)
-                # merge those in
-                self.arns.extend(
-                    [d.get(instance.id_key, []) for d in result]
-                )
-                tag_field = self._tags.get(key, None)
-                if tag_field is not None:
-                    untagged.extend( self._untagged(result, instance.id_key, tag_field))
-                else:
-                    untagged.extend( self._untagged(result, instance.id_key))
-                    
-        pprint(untagged)
-        return {'untagged': untagged, 'all': self.arns}
+    
 
     
--- untagged-resources/custom_aws/services/service_base.py
+++ untagged-resources/custom_aws/services/service_base.py
@@ -1,6 +1,16 @@
+import aioboto3
+from pprintpp import pprint
+
 class ServiceBase:
     """Base class for AWS service calls"""
     arns: list = []
+    client_type:str = ""
+
+    _find:dict = {}
+    _kwargs:dict = {}
+    _tag_names:dict = {}
+
+
 
     def _untagged(self, dataset:list, id_key:str, tag_key:str = 'tags') -> list:
         """Return arn that arent tagged """
@@ -14,6 +24,28 @@ class ServiceBase:
 
 
     async def all(self, region_name:str) -> dict:
-        """Return a list of all data from this service"""
+        """Return all the arns of all resources in the client_type scope that don't contain tags"""
+        untagged = []
+        session = aioboto3.Session(region_name=region_name)
 
-        return {}
+        async with session.client(self.client_type, region_name=region_name) as client:
+            for key, instance in self._find.items():
+                kwargs = self._kwargs.get(key, None)
+                # check if we need to pass extra params
+                if kwargs is not None:
+                    result = await instance.call(client, **kwargs)
+                else:
+                    result = await instance.call(client)
+                # merge those in
+                self.arns.extend(
+                    [d.get(instance.id_key, []) for d in result]
+                )
+                # check if this uses a different name for tags
+                tag_field = self._tag_names.get(key, None)
+                if tag_field is not None:
+                    untagged.extend( self._untagged(result, instance.id_key, tag_field))
+                else:
+                    untagged.extend( self._untagged(result, instance.id_key))
+
+        pprint(untagged)
+        return {'untagged': untagged, 'all': self.arns}
