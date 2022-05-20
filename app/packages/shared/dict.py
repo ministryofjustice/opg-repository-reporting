5b910709256c7170c0bce84b02f3368ad95faba1
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2022-05-20 13:41:41 +0100

    Add totals line at the bottom

--- app/packages/shared/dict.py
+++ app/packages/shared/dict.py
@@ -4,7 +4,7 @@ from .logger.out import Out
 
 
 def counters_for_date_range(start:date, end:date, struct:dict) -> dict:
-    """ Returns an empty dict with preset keys referencing YYYYMM between the start and end date """
+    """ Returns an empty dict with preset keys referencing YYYY-MM between the start and end date """
     # make sure start is the first day of the month
     start = start.replace(day=1, hour=0, minute=0, second=0)
     Out.debug(f"Creating dict between [{start}] and [{end}]")
